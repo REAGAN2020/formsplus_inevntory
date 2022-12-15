3c91073a0eb809e467772855f7d86f82c181a50d
REAGAN2020 <reaganteflon@gmail.com>
2022-12-15 10:17:46 +0000

    changed the database settings

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'inventory_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_database',
+        'USER': 'my_user',
+        'PASSWORD': 'my_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView, LabelListCreateAPIView, LabelRetrieveUpdateDestroyAPIView, CartListView, CartDetailView
+from django.urls import path,re_path
+from api.views import ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView, LabelListCreateAPIView, LabelRetrieveUpdateDestroyAPIView, CartListView, CartDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
