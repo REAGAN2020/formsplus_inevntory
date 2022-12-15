39adf87477ab040f2d0cfdf49aea70072d11576a
REAGAN2020 <reaganteflon@gmail.com>
2022-12-15 08:15:05 +0000

    made a small change on serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import Product, Label, Cart
 
 class ProductSerializer(serializers.ModelSerializer):
     labels = serializers.StringRelatedField(many=True)
