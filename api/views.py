46efe9d77d50259466e59a694f29a1ee902dbfc3
REAGAN2020 <reaganteflon@gmail.com>
2022-12-15 07:52:16 +0000

    added class cart on views

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
 from rest_framework import generics
+from .serializers import ProductSerializer, LabelSerializer, CartSerializer
+from .models import Product, Label, Cart
+
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -18,4 +20,10 @@ class LabelRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Label.objects.all()
     serializer_class = LabelSerializer
 
-# class CartList
+class CartListView(generics.ListAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class CartDetailView(generics.RetrieveAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
