42ae5b39b69d0640b257b794f5ae887044d02ddc
reagan <reaganteflon@gmail.com>
2022-12-16 07:14:36 +0300

    modified views and urls

--- api/views.py
+++ api/views.py
@@ -1,82 +1,21 @@
 from django.shortcuts import render
-from rest_framework import generics
+from django.http import JsonResponse
 from .serializers import ProductSerializer, LabelSerializer, CartSerializer
 from .models import Product, Label, Cart
 
-
-class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    
-    def post(self, request, *args, **kwargs):
-        # Get the user's cart
-        cart = self.get_cart(request)
-
-        # Get the product to add to the cart
-        product = self.get_object()
-
-        # Add the product to the cart
-        cart.add_product(product)
-
-        # Return the updated cart
-        serializer = self.get_serializer(cart)
-        return Response(serializer.data)
-
-class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    
-    def put(self, request, *args, **kwargs):
-        # Get the user's cart
-        cart = self.get_cart(request)
-
-        # Get the product to update in the cart
-        product = self.get_object()
-
-        # Update the product in the cart
-        cart.update_product(product)
-
-        # Return the updated cart
-        serializer = self.get_serializer(cart)
-        return Response(serializer.data)
-
-    def delete(self, request, *args, **kwargs):
-        # Get the user's cart
-        cart = self.get_cart(request)
-
-        # Get the product to remove from the cart
-        product = self.get_object()
-
-        # Remove the product from the cart
-        cart.remove_product(product)
-
-        # Return the updated cart
-        serializer = self.get_serializer(cart)
-        return Response(serializer.data)
-
-class LabelListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Label.objects.all()
-    serializer_class = LabelSerializer
-
-class LabelRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Label.objects.all()
-    serializer_class = LabelSerializer
-
-class CartListView(generics.ListAPIView):
-    queryset = Cart.objects.all()
-    serializer_class = CartSerializer
-    
-    def post(self, request, *args, **kwargs):
-        # Get the user's cart
-        cart = self.get_cart(request)
-
-        # Purchase the products in the cart
-        cart.purchase()
-
-        # Return the updated cart
-        serializer = self.get_serializer(cart)
-        return Response(serializer.data)
-
-class CartDetailView(generics.RetrieveAPIView):
-    queryset = Cart.objects.all()
-    serializer_class = CartSerializer
+def products_view(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        products_list = [{'name': product.name, 'category': product.category, 'labels': [label.name for label in product.labels.all()], 'quantity': product.quantity, 'price': str(product.price)} for product in products]
+        return JsonResponse({'products': products_list})
+    elif request.method == 'POST':
+        data = request.POST
+        name = data.get('name')
+        category = data.get('category')
+        labels = data.get('labels')
+        quantity = data.get('quantity')
+        price = data.get('price')
+        product = Product.objects.create(name=name, category=category, quantity=quantity, price=price)
+        product.labels.set(labels)
+        product.save()
+        return JsonResponse({'message': 'Product created successfully.'})
